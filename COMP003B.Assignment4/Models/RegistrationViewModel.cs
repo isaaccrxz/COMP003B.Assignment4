27cabf7e57b078cf77b4cd606a2ee4bbb6260532
isaaccrxz <157342207+isaaccrxz@users.noreply.github.com>
2024-04-19 19:07:57 -0700

    Add project files.

---
+++ COMP003B.Assignment4/Models/RegistrationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.Assignment4.Models
+{
+    public class RegistrationViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        [StringLength (100)]
+        public string Address { get; set; }
+    }
+}
