92651cec4af12ffc7d6607ae64b90c49d8418d83
isaaccrxz <157342207+isaaccrxz@users.noreply.github.com>
2024-04-19 20:59:58 -0700

    Made RegistrationViewModel and added all the strings in it

---
+++ COMP003B.Assignment4/Controllers/RegisterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment4.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
